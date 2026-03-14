e99002dca8c02b932d39bdbe0d23848b9b415f03
Sajlendra Pandey <sajlendrapandey2022@gmail.com>
2026-03-15 03:07:04 +0530

    secured credentials with env

--- wastenot_project/settings.py
+++ wastenot_project/settings.py
@@ -133,5 +133,5 @@ EMAIL_BACKEND      = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST         = 'smtp.gmail.com'
 EMAIL_PORT         = 587
 EMAIL_USE_TLS      = True
-EMAIL_HOST_USER    = 'yashpandey2023u@gmail.com'
-EMAIL_HOST_PASSWORD = 'wrug xtri fgew ssoz'
+EMAIL_HOST_USER     = os.getenv('EMAIL_HOST_USER')      
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD') 
