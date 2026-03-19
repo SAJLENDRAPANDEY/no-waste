77d61427255c148f59da54f31b568d13488a63cc
Sajlendra Pandey <sajlendrapandey2022@gmail.com>
2026-03-19 18:39:42 +0530

    fix SECRET_KEY issue

--- wastenot_project/settings.py
+++ wastenot_project/settings.py
@@ -6,7 +6,8 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY')  
+SECRET_KEY = os.getenv('SECRET_KEY', 'fallback-secret-key')
+DEBUG = True   
 
 # DEBUG = os.getenv('DEBUG') == 'True'
 
